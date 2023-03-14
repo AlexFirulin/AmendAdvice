faaaa86a18e7a2d9804026468cdffabb5f5b4bbc
AlexFirulin <117468025+AlexFirulin@users.noreply.github.com>
2023-03-14 12:32:35 +0200

    Correct css

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
-  <div class="bg-black min-h-screen">
+  <div class="bg-black min-h-screen max-h-screen">
     <div class="header">
-      <h1 class="header"><img src="@/assets/images/Logo.png" alt="/" srcset=""></h1>
+      <img src="@/assets/images/Logo.png" alt="/" srcset="">
     </div>
 
     <div class="container flex flex-col">
       <div class="main flex flex-col">
-      <img src="@/assets/images/Frame16.png" alt="My Image" class="mt-28 mr-56">
-      <div class="message"> &#x1F32E; &#x2049;😱
-
-</div>
-      <img src="@/assets/images/Frame21.png" alt="My Image" class="mt-8 ml-56">
-      <div class="answear">🤔💪🤳</div>
-    </div>
-
-      <div class="text-container mt-8">
-        <span class="text-green">ывфвыфвфывфыыфв</span>
-        <h1 class="text-white mt-4 mb-2 font-bold text-3xl md:text-4xl">Instant help with anything</h1>
+        <img src="@/assets/images/Frame16.png" alt="My Image" class="mt-10 md:mt-28 mr-4 md:mr-56">
+        <div class="message"> &#x1F32E; &#x2049;😱 </div>
+        <img src="@/assets/images/Frame21.png" alt="My Image" class="mt-2 md:mt-8 ml-4 md:ml-56">
+        <div class="answear">🤔💪🤳</div>
       </div>
 
-      <button class="mt-10 bg-blue flex items-center justify-center text-white">
-        <span class="text-xl font-bold" >NEXT -&gt;</span>
+      <div class="text-container mt-2 md:mt-8">
+        <span class="text-green">Help on demand</span>
+        <h1 class="text-white mt-2 md:mt-4 mb-2 font-bold text-2xl md:text-4xl">Instant help with anything</h1>
+      </div>
+
+    <router-link to="LoginPage">
+    <div class="flex justify-center mt-5 bg-black">
+
+    <button class="btn  text-white">
+
+  <span class="text-xl font-bold">NEXT-&gt;</span>
       </button>
     </div>
-
+    </router-link>
+    </div>
     <router-view />
   </div>
 </template>
+
+<script>
+</script>
 
 
 <style lang="scss">
@@ -41,18 +47,19 @@
   }
 
   .bg-black {
+    width: auto;
     background-color: black;
+    height: auto;
   }
 
-
-.header {
-  height: 375px;
-  display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  padding-top:7vh;
-}
-
+  .header {
+    box-sizing: border-box;
+    height: 20vw;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    padding-top: 3vh;
+  }
 
   .container {
     max-width: 1200px;
@@ -61,7 +68,7 @@
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
-    margin-top: -200px;
+    margin-top: -220px;
     position: relative;
     z-index: 1;
   }
@@ -71,77 +78,75 @@
     height: auto;
     background-color: black;
   }
+
   .message {
-  background-color: yellow;
-  z-index: 2;
-  border-radius: 32px;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 8px;
-  gap: 10px;
-  width: 98px;
-  height: 44px;
-  position: absolute;
-  top: 95px;
-  right: 770px;
-}
-.answear{
-  display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
-padding: 8px;
-gap: 10px;
-z-index: 2;
+    background-color: yellow;
+    border-radius: 32px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 8px;
+    gap: 10px;
+    width: 98px;
+    height: 44px;
+    position: relative;
+    top: -85px;
+    right: -55px;
+    z-index: 2;
+  }
 
-position: absolute;
-width: 98px;
-height: 44px;
-right: 670px;
-top: 210px;
-
-background: #99F96C;
-border-radius: 32px;
-}
-
+  .answear {
+    background: #99F96C;
+    border-radius: 32px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 8px;
+    gap: 10px;
+    width: 98px;
+    height: 44px;
+    position: relative;
+    right: -160px;
+    top: -90px;
+    z-index: 2;
+  }
 
   .text-container {
-  margin-top: 80px;
-  text-align: left;
   display: flex;
-  align-self: start;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-left: 30%;
-
 }
 
   span.text-green {
     color: var(--color-secondary);
+    display: flex;
   }
 
   h1 {
     font-size: var(--font-size-large);
-    margin-top: 0;
-    margin-bottom: 0;
+    display: flex;
+    position: relative;
+    top: 1px;
   }
 
-  button {
-    width: var(--button-width);
-    height: var(--button-height);
-    border-radius: var(--button-border-radius);
-    background-color: var(--color-primary);
-    margin-top: 50px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border: none;
-    position: relative;
-    z-index: 1;
-  }
+
+  .btn {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 16px 8px;
+  background-color: #5843F4;
+  gap: 10px;
+  border-radius: 150vh;
+  z-index: 1;
+  border-radius: 16px;
+  position: absolute;
+width: 295px;
+height: 60px;
+}
+
+
 
   button span {
     font-size: var(--font-size-medium);
